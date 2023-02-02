b9b469dd3c78ba540ae3b9c2d495fba2dff236d1
Ben Fenocchi <bf354@cam.ac.uk>
2023-02-02 13:11:42 +0000

    Task 1D done

---
+++ Task1D-Ben.py
@@ -0,0 +1,61 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_with_station, stations_by_river
+
+
+
+def run():
+    """Requirements for Task 1D"""
+    """
+    Uses geo.rivers_with_station to print how many rivers have at least one monitoring station (Representative result: 843) 
+    and prints the first 10 of these rivers in alphabetical order. 
+
+    Uses geo.stations_by_river to print the names of the stations 
+    located on the following rivers in alphabetical order
+    """
+    stations = build_station_list()
+
+    riverStations = rivers_with_station(stations)
+    riverStations.sort()#sorts to alphabetical so i dont have to later
+    counter = 0
+    riversWithMoreThanOneStation = []
+
+    for river in riverStations:#goes through all the rivers with stations
+        for station in stations:#looks for all the stations on this river
+            if station.river == river:
+                counter+=1#if the station is on the river then add to a counter
+        if counter >1 :#if there is more than one station on the river
+            riversWithMoreThanOneStation.append(river)#add it to the list
+        counter = 0#reset for the next list
+
+    print(len(riversWithMoreThanOneStation),"rivers with multiple stations")
+    print("The first ten are",riversWithMoreThanOneStation[:10])
+
+
+
+    riverStationDict = stations_by_river(stations)
+    list = []
+    for x in riverStationDict["River Aire"]:
+        list.append(x.name)
+        list.sort()
+    print(list)
+    list = []
+    for x in riverStationDict["River Cam"]:
+        list.append(x.name)
+        list.sort()
+    print(list)
+    list = []
+    for x in riverStationDict["River Thames"]:
+        list.append(x.name)
+        list.sort()
+    print(list)
+
+
+
+
+
+    
+
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IB Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,6 +9,29 @@ geographical data.
 from .utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
 
+
+def rivers_with_station(stations):
+    rivers = []
+    for station in stations:
+        river = station.river
+        if not river in rivers:
+            rivers.append(river)
+    return(rivers)#returns a list of the rivers that have stations with no duplicates
+
+def stations_by_river(stations):
+    riverNames = rivers_with_station(stations)#retrieves a list of the rivers with stations
+    riverDict = {}
+    stationsOnRiver = []
+    for river in riverNames:#goes through all the rivers
+        for station in stations:#looks for all stations on this river
+            if station.river == river:#if it finds a station on the river
+                stationsOnRiver.append(station)#adds it to a list of stations for this river
+        riverDict[river] = stationsOnRiver#adds it to the dictionary
+        stationsOnRiver = []#resets the list for the next river
+    return(riverDict)
+
+
+
 def stations_by_distance(stations, p):
     """
     takes list of station objects and a co-ordinate p as an input
