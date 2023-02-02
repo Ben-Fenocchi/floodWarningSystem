0aea6d872e59d1fdff807a884c1f7bb3938a43c1
Ben Fenocchi <bf354@cam.ac.uk>
2023-02-02 11:27:12 +0000

    Task1B completed

---
+++ Task1B-Ben.py
@@ -0,0 +1,39 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
+
+def run():
+    """Requirements for Task 1B"""
+    """
+    prints a list of tuples (station name, town, distance)
+    for the 10 closest and the 10 furthest stations from the Cambridge city centre
+    """
+
+    camCityCentre = (52.2053, 0.1218)
+    stations = build_station_list() # collects list of stations
+    stationsByDistance = stations_by_distance(stations,camCityCentre)  #returns list of stations and their distances
+
+    closestStations = []
+    furthestStations = []
+    closest = stationsByDistance[:10]
+    furthest = stationsByDistance[-10:]
+    #print(closest)
+    #print(furthest)
+    
+    for item in closest:
+        currentTuple = tuple((item[0].name, item[0].town, item[1]))
+        closestStations.append(currentTuple)        
+
+    furthest = stationsByDistance[-10:]
+    for item in furthest:
+        currentTuple = tuple((item[0].name, item[0].town, item[1]))
+        furthestStations.append(currentTuple)
+ 
+        
+
+    print(closestStations)
+    print(furthestStations)
+    
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IB Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+def stations_by_distance(stations, p):
+    """
+    takes list of station objects and a co-ordinate p as an input
+    then returns a list of tuples (station,distance)
+    """
+    stationsByDistanceList = []
+    for station in stations:
+        distance = haversine(station.coord,p)#inputs to this are lat and long tuples for each place
+        data = [station,distance]
+        stationsByDistanceList.append(data)
+
+    return sorted_by_key(stationsByDistanceList,-1)#list of lists to sort, index of sublist sorting by, -1 puts to the end which is where my distance haversine is
