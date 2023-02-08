0a4ca2c7fcf85d5afc2c4b80c868f8aec34ad636
LiamMartin-1 <123095302+LiamMartin-1@users.noreply.github.com>
2023-02-08 00:49:08 +0000

    Update Task1C-Liam.py

--- Task1C-Liam.py
+++ Task1C-Liam.py
@@ -18,5 +18,5 @@ def run():
   
   
 if __name__ == "__main__":
-    print("*** Task 1B: CUED Part IB Flood Warning System ***")
+    print("*** Task 1C: CUED Part IC Flood Warning System ***")
     run()
