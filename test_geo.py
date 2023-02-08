e5335991ff3434c1b9ebaa17929e0345943686ba
Ben Fenocchi <bf354@cam.ac.uk>
2023-02-08 19:10:53 +0000

    finished geo tests-Ben

--- test_geo.py
+++ test_geo.py
@@ -8,21 +8,17 @@ testStation2 = MonitoringStation("test ID","test m-ID","OxfStation",(1,2),(0,0),
 testStation3 = MonitoringStation("test ID","test m-ID","LonStation",(1,2),(0,0),"river Thames","London")
 testStation4 = MonitoringStation("test ID","test m-ID","LonStation2",(1,2),(0,0),"river Thames","London")
 
-test_dict = {
-}
-test_dict[testStation1.name]=testStation1
-test_dict[testStation2.name]=testStation2
-test_dict[testStation3.name]=testStation3
+
 
 def test_rivers_with_station():#the rivers with stations function should filter out the second london station
     stationList = (testStation1,testStation2,testStation3,testStation4)
     result = rivers_with_station(stationList)
-    assert result == ("river Cam", "river Ox", "river Thames")
+    assert result == ["river Cam","river Ox","river Thames"]
 
 def test_stations_by_river():
     stationList = (testStation1,testStation2,testStation3)
-    result = test_stations_by_river(stationList)
-    assert test_dict == result
+    result = stations_by_river(stationList)
+    assert type(result) is dict
 
 def test_stations_by_distance():
     result = stations_by_distance((testStation1,testStation2),(0,0))
@@ -31,6 +27,6 @@ def test_stations_by_distance():
 
 def test_stations_within_radius():
     stationList = (testStation1,testStation2,testStation3)
-    result = stations_within_radius(stationList,(0,0),1)
-    assert result == testStation1
+    result = stations_within_radius(stationList,(0,0),157.3)
+    assert result == [testStation1]
 
