54ab7681491166ae94902a0bcfb69285e135e028
LiamMartin-1 <123095302+LiamMartin-1@users.noreply.github.com>
2023-02-08 11:24:45 +0000

    Update Task1C.py

--- Task1C.py
+++ Task1C.py
@@ -2,23 +2,17 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 
 def run():
-  print("task 1C start")
-  #Provide a program file Task1C.py that uses the function geo.stations_within_radius to build a list of stations
-  #within 10 km of the Cambridge city centre (coordinate (52.2053, 0.1218)). Print the names of the stations,
-  #listed in alphabetical order. Representative output:
-  camCityCentre = (52.2053, 0.1218)
-  radius = 10
-  stations = build_station_list()
-  StationsWithinRadius = stations_within_radius(stations,camCityCentre, radius)
-  StationsWithinRadius.sort
-  for Stations in StationsWithinRadius:
-    print(Stations)
-  print("task 1C End")
-  
-  
-  
+    print("task 1C start")
+    camCityCentre = (52.2053, 0.1218)
+    radius = 10
+    stations = build_station_list()
+    StationsWithinRadius = stations_within_radius(stations,camCityCentre, radius)
+    StationsWithinRadius.sort()
+    for Stations in StationsWithinRadius:
+        print(Stations)
+        print("task 1C End")
   
   
 if __name__ == "__main__":
-   print("*** Task 1C: CUED Part IC Flood Warning System ***")
-   run()
+    print("*** Task 1C: CUED Part IC Flood Warning System ***")
+    run()
