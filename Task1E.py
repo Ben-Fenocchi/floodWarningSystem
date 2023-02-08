7cc2bc8d31c33be06c739a1bde56fd42578609be
LiamMartin-1 <123095302+LiamMartin-1@users.noreply.github.com>
2023-02-08 20:25:17 +0000

    Task 1E done

--- Task1E.py
+++ Task1E.py
@@ -1,5 +1,6 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
+from floodsystem.utils import sorted_by_key
 
 def run():
     number = 9
@@ -8,5 +9,5 @@ def run():
     
     
 if __name__ == "__main__":
-print("*** Task 1E: CUED Part IA Flood Warning System ***")
-run()
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run()
