07a84290b29f882b3a9b9b7ed0a9387d420ff514
LiamMartin-1 <123095302+LiamMartin-1@users.noreply.github.com>
2023-02-08 11:02:28 +0000

    Create Task1E-Liam.py

---
+++ Task1E-Liam.py
@@ -0,0 +1,12 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+
+def run():
+    number = 9
+    stations = build_station_list()
+    print(rivers_by_station_number(stations, number))
+    
+    
+if __name__ == "__main__":
+print("*** Task 1E: CUED Part IA Flood Warning System ***")
+run()
