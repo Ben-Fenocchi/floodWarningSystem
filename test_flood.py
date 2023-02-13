2247b5b7d339c5db09c2a06a7d0866c689392cd4
Ben Fenocchi <bf354@cam.ac.uk>
2023-02-13 15:16:58 +0000

    Finished 2b+tests-Ben

---
+++ test_flood.py
@@ -0,0 +1,19 @@
+from floodsystem.station import MonitoringStation
+from floodsystem.flood import stations_level_over_threshold
+
+testStation1 = MonitoringStation("test ID","test m-ID","CamStation",(1,1),(0,1.0),"river Cam","Cambridge")
+testStation2 = MonitoringStation("test ID","test m-ID","OxfStation",(1,2),(0,1.0),"river Ox","Oxford")
+testStation3 = MonitoringStation("test ID","test m-ID","LonStation",(1,2),(0,1.0),"river Thames","London")
+testStation4 = MonitoringStation("test ID","test m-ID","LonStation2",(1,2),(0,1.0),"river Thames","London")
+
+testStation1.latest_level = 0# this is the low range
+testStation2.latest_level = 0.5 #this is the mid range
+testStation3.latest_level = 0.81# this is slightly over 0.8 of high range
+testStation4.latest_level = 0.8
+
+stationList = [testStation1,testStation2,testStation3,testStation4]
+
+def test_stations_level_over_threshold():
+    highRiskStations = stations_level_over_threshold(stationList,0.79)
+    print(highRiskStations)
+    assert highRiskStations == [(testStation3,0.81),(testStation4,0.8)]
--- test_station.py
+++ test_station.py
@@ -5,6 +5,7 @@
 
 from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 
+
 inconsistentStation1= MonitoringStation("test ID","test m-ID","CamStation",(1,1),None,"river Cam","Cambridge")
 inconsistentStation2=  MonitoringStation("test ID","test m-ID","OxStation",(1,1),(1,0),"river Ox","Oxford")
 consistentStation =  MonitoringStation("test ID","test m-ID","LonStation",(1,1),(0,0),"river Thames","London")
@@ -34,3 +35,9 @@ def test_inconsistent_typical_range_stations():
     result = inconsistent_typical_range_stations(stationList)
     assert result == [inconsistentStation1,inconsistentStation2]
     assert type(result) is list 
+
+def test_relative_water_level():
+    testStation=  MonitoringStation("test ID","test m-ID","OxStation",(1,1),(0,1),"river Ox","Oxford")
+    testStation.latest_level = 0.5
+    result = testStation.relative_water_level()
+    assert result == 0.5
