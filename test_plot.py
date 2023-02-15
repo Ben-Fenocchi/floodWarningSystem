ad711d36ef5eb6cc5a32455916c5d4abcde32406
Ben Fenocchi <bf354@cam.ac.uk>
2023-02-15 14:35:20 +0000

    written test for 2E(have not done demonstration program yet)

--- test_plot.py
+++ test_plot.py
@@ -1,6 +1,12 @@
 from floodsystem.plot import plot_water_levels
+from floodsystem.station import MonitoringStation
 
-
+testStation4 = MonitoringStation("test ID","test m-ID","LonStation2",(1,2),(0,1.0),"river Thames","London")
+dates = [12,13,14,15,16,17]
+levels = [1.0,1.29,1.3,2.7,2.45,4.56]
 
 def test_plot_water_levels():
-    pass
+    result = plot_water_levels(testStation4,dates,levels)
+    #This test checks that the function will accept the expected datatypes, as otherwise an error will
+    #be thrown up causing the test to fail
+    
