bf12cc30ad39cc1c94d86a0e01829e12002b4996
Ben Fenocchi <bf354@cam.ac.uk>
2023-02-15 20:57:21 +0000

    finished 2E

--- Task2E-Ben.py
+++ Task2E-Ben.py
@@ -2,17 +2,19 @@ from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.plot import plot_water_levels
+from floodsystem.stationdata import update_water_levels
 import datetime
 
 def run():
     stations = build_station_list()
+    update_water_levels(stations)
     necessaryStations = stations_highest_rel_level(stations,5)
     necessaryStations = necessaryStations[:5]
     #takes the highest 5 stations
 
-    for station in necessaryStations:
-        dates,levels = fetch_measure_levels(station.measure_id, datetime.timedelta(days=10))
-        plot_water_levels(station,dates,levels)
+    for data in necessaryStations:
+        dates,levels = fetch_measure_levels(data[0].measure_id, datetime.timedelta(days=10))#data[0] is the station, data[1] is the risk level thingy
+        plot_water_levels(data[0],dates,levels)
 
 
 if __name__ == "__main__":
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
+import math
 
 
 def plot_water_levels(station, dates, levels):
+
     #plot
     plt.plot(dates,levels)
-
+    plt.axhline(y= station.typical_range[1],color ="r")
+    plt.axhline(y= station.typical_range[0],color ="g")
     # Add axis labels, rotate date labels and add plot title
     plt.xlabel('date')
     plt.ylabel('water level (m)')
@@ -14,4 +17,4 @@ def plot_water_levels(station, dates, levels):
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
 
-    return(plt)
+    plt.show()
